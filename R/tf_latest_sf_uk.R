9ee9143b0cd00c1d17f1a6a0f0a508ad24906a43
julianflowers <julianflowers@users.noreply.github.com>
2023-08-11 10:42:01 +0100

    updates

---
+++ R/tf_latest_sf_uk.R
@@ -0,0 +1,30 @@
+#' Core data for UK Tiny Forests
+#'
+#'
+#'Tiny Forests are small scale densely planted wooded areas stocked with 
+#'native trees and understory plants. They rarely exceed 200m2 and are planted in urban/ semi-urban areas
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'@source \url{https://tinyforest.earthwatch.org.uk/tiny-forest-sites}
+
+
+
+
+
+
+
+
+
+
+
+
+
+"tf_latest_sf_uk"
