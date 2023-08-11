5beaf47b9f131057bdcdcdbbca07c1c5e7349088
julianflowers <julianflowers@users.noreply.github.com>
2023-08-11 18:20:18 +0100

    rainfall

---
+++ R/seasonal_rainfall_tf.R
@@ -0,0 +1,18 @@
+#' Quarterly rainfall for  each Tiny Forest from 2010
+#'
+#'
+#'Derived from CEDA 1km gridded rainfall data assigned to and averaged over each Tiny Forest buffer
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'\ref{Met Office (2022): MIDAS Open: UK daily rainfall data, v202207. NERC EDS Centre for Environmental Data Analysis, 22 September 2022. doi:10.5285/15deeb29cdcd4524b07560e5aad45ded. https://dx.doi.org/10.5285/15deeb29cdcd4524b07560e5aad45ded}
+#'
+#'
+#'@source \url{https://catalogue.ceda.ac.uk/uuid/15deeb29cdcd4524b07560e5aad45ded}
+
+
+"seasonal_rainfall_tf"
