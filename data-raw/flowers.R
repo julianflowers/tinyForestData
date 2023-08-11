3f5e671e2c64aa3b96b8b518511d081f2acf6743
julianflowers <julianflowers@users.noreply.github.com>
2023-08-11 13:51:10 +0100

    removed data

--- data-raw/flowers.R
+++
@@ -1,5 +0,0 @@
-## code to prepare `flowers` dataset goes here
-
-load("~/Library/CloudStorage/Dropbox/Mac (2)/Desktop/tinyForestR/data/flowers_filt.rda")
-
-usethis::use_data(flowers_filt, overwrite = TRUE)
