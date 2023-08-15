477e7e44f954fa7a35a3a8ae916cf10abd16b106
julianflowers <julianflowers@users.noreply.github.com>
2023-08-15 08:08:23 +0100

    dynamic world landcover

--- R/tf_dw_counts.R
+++ R/tf_dw_counts.R
@@ -1,10 +1,10 @@
 #' Pixel land cover counts per Tiny Forest
 #'
+#' Modal pixel assignment to dynamic world categories using images from 2022-01-01 to 2023-08-01
 #'
 #'
 #'
-#'
-#'
+#'@source \url{https://developers.google.com/earth-engine/datasets/catalog/GOOGLE_DYNAMICWORLD_V1}
 
 
 "tf_dw_counts"
--- data-raw/tf_dw_counts.R
+++ data-raw/tf_dw_counts.R
@@ -1,5 +1,5 @@
 ## code to prepare `tf_dw_counts` dataset goes here
 
-tf_dw_counts <- read_sf("tf_dw_counts.geojson")
+tf_dw_counts <- read_sf("tf_dw_counts_1.geojson")
 
 usethis::use_data(tf_dw_counts, overwrite = TRUE)
